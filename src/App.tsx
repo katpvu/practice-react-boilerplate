4cd9768d5968794eae1185b0c792daaa1cba037d
Kat Vu <vu.kathy98@gmail.com>
2025-05-22 22:09:47 -0700

    vite, react, typescript, tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen w-screen bg-white text-black flex items-center justify-center">
+        <div className="max-w-4xl w-full px-4 text-center">
+          <h1 className="text-4xl font-bold mb-4">Hello world 👋</h1>
+          <p className="text-lg text-gray-600">
+            Boilerplate for Practicing React Apps!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
